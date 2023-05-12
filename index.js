e2cfd4453322ad4ede62c3e82fc8919d74ef20ca
Bora Kol <83176378+BoraKol@users.noreply.github.com>
2023-05-12 23:24:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ else if(randomNumber1===randomNumber2) {
 }
 
 else {
-    document.querySelector("h1").innerHTML="🚩 Player 2 Wins!"
+    document.querySelector("h1").innerHTML="Player 2 Wins! 🚩"
 }
 
 
